071ecbfcf9eb4d6968fb67207af1ad463f96f6b0
yasserelshabrawy <yasser.elshabrawy98@gmail.com>
2023-11-20 18:29:48 +0200

    addToCart

--- src/app/Component/product-details/product-details.component.ts
+++ src/app/Component/product-details/product-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/Services/cart.service';
 import { GetProductService } from 'src/app/Services/get-product.service';
 
 @Component({
@@ -9,19 +11,43 @@ import { GetProductService } from 'src/app/Services/get-product.service';
 })
 export class ProductDetailsComponent implements OnInit {
   prdLis: any;
+  toast: any;
+  
+
   constructor(
     private prdService: GetProductService,
-    private avtivateRoute: ActivatedRoute
-  ) {}
+    private avtivateRoute: ActivatedRoute,
+    private cart: CartService,
+    private snackBar: MatSnackBar,
+    ) {
+
+  }
 
   ngOnInit(): void {
-    let productId = this.avtivateRoute.snapshot.paramMap.get('productId');
+     let productId = this.avtivateRoute.snapshot.paramMap.get('productId');
     console.log(productId);
-    this.productsDetails(productId)
+
+    this.productsDetails(productId);
+
   }
-  productsDetails(productId:any) {
+  addToCart(id: string) {
+    this.cart.addToCart(id).subscribe((add) => {
+      this.cart.numOfCartItems.next(add.numOfCartItems);
+      this.toast = add;
+      console.log(this.toast.message);
+      this.snackBar.open(this.toast.message, 'undo', {
+        duration: 1000,
+        verticalPosition: 'top',
+        horizontalPosition: 'center',
+        panelClass: 'my-custom-snackbar',
+      });
+    });
+  }
+  productsDetails(productId: any) {
     this.prdService.getProductsDetails(productId).subscribe((details) => {
       this.prdLis = details;
+
+
       console.log(this.prdLis);
     });
   }
